61c6d0c2dd184b0950658e5bdcdfadf642d8c0b6
Kroos <hamroliyevahmadjon1909@gmail.com>
2024-03-03 11:01:38 +0500

    Create Contact Model.

---
+++ PhoneBooks/Models/Contact.cs
@@ -0,0 +1,9 @@
+﻿namespace PhoneBooks.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
